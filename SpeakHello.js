7382e28fabe771c4b45c4841ca3ad0e4b781e3e7
sanjeed <sanjeedev2@gmail.com>
2020-06-11 12:53:55 +0530

    Create SpeakHello.js

---
+++ SpeakHello.js
@@ -0,0 +1,21 @@
+(function (window) {
+  
+  
+  
+  
+  var helloSpeaker = {};
+  
+  
+  var speakWord = "Hello";
+  
+  
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+  
+  
+  window.helloSpeaker = helloSpeaker;
+  
+  })(window);
+
+
